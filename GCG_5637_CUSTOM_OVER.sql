ceac567a33b1703410930eae360c769282b262db
Service Web <sw@gssmail.com>
2024-03-14 13:53:33 -0500

    Commit changes in C:\Websites\Git\emarlow\5637\GCG_5637_CUSTOM_OVER.sql

---
+++ GCG_5637_CUSTOM_OVER.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_5637_CUSTOM_OVER
+ * ========================================== */
+CREATE TABLE "GCG_5637_CUSTOM_OVER"(
+ "WORKCENTER" CHAR(4),
+ "MACH_OVHD" NUMERIC(8,2),
+ "BLDG_OVHD" NUMERIC(8,2),
+ "LAST_MOD_DATE" DATE,
+ "LAST_MOD_USER" CHAR(9));
+
+
